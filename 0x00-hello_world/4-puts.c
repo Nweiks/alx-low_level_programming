dcc56a0b81dda0c2756092541f5c9aa6742bc598
nweiks <nweikeebube@gmail.com>
2023-03-17 07:11:07 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return A;ways 0 (success)
+ */
+int main(void)
+{
+	char string[] = "Programming is like building a multilingual puzzle";
+
+	puts(string); //writing the string to stout
+
+	return (0);
+}
