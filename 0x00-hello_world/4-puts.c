8c2ee83f0881513d5a86c63464ef849f35e919d3
nweiks <nweikeebube@gmail.com>
2023-03-17 07:42:56 -0700

    use puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return Always 0 (success)
+ * return Always 0 (success)
  */
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
