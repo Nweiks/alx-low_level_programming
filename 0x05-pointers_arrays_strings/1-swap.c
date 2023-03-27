91fa5315faf82f09ff6a331d29d774f92cf2912e
nweiks <nweikeebube@gmail.com>
2023-03-27 11:18:05 -0700

    swap_integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*swai_int - swaops two varables
+*@a: value 1
+*@b: value 2 to be swapped with 1
+*Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
