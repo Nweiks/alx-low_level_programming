98f2bb0c9151bc20db891e7e9e36012bc88d4452
nweiks <nweikeebube@gmail.com>
2023-03-28 17:08:15 -0700

    other string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *puts2 - main 
- *@str: string tp be printed
+*puts2 - main 
+*@str: string tp be printed
  */
 
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; i < (*str); i+=2)
+	for (i = 0; i < (str); i += 2)
 	{
 		_putchar(str[i]);
 	}
