885ff8d2f8e6c44894f331e79c1a94092ab75908
nweiks <nweikeebube@gmail.com>
2023-03-28 17:48:14 -0700

    print-half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *puts_half - main
+ *@str: string to be printed
+ *Return: 0
  */
 
 void puts_half(char *str)
 {
 	int j = 0;
+int i;
 
 	while (str[j] != '\0')
 	{
 		j++;
 	}
-	if (j % 2 != 0)
+	if (i % 2 != 0)
 	{
-		_putchar(str[j - 1]/ 2);
+		_putchar(str[i]- 1 / 2);
 	}
 	else
 	{
-		_putchar(str[j]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
