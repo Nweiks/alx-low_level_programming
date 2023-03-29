979f996980f5818a3476059c9cf6878189688d45
nweiks <nweikeebube@gmail.com>
2023-03-28 18:53:10 -0700

    password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+*Return: 0
+ */
+
+int main(void)
+{
+	int c, n;
+	printf("Your password is\n");
+
+	for (c = 1; c <= 1; c++)
+	{
+		n = rand()%9999 + 1000;
+		printf("%d\n", n);
+	}
+	return (0);
+}
