44878a3bcc582f798b6247a053a9377ba2605a16
nweiks <nweikeebube@gmail.com>
2023-03-30 22:30:08 +0000

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,31 @@
 #include "main.h"
 
 /**
-*
-*
-*
-*/
+ *_strncat - main string to be merged
+ *@dest: string 1
+ *@src: string 2
+*Return: dest
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i:
-int j;
+	int i;
+	int j;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++
-}
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-j = 0;
-while (j < n && src[j] != '\0')
-{
-i++;
-j++;
-dest[i] = src[j];
-}
-dest[i] = '\0';
-return (dest);
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		i++;
+		j++;
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
