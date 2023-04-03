7d50a24f16ee76be2c88ed43d717be1e33608230
nweiks <nweikeebube@gmail.com>
2023-04-03 23:11:45 +0000

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
-/*
+/**
  *_memset - set value
  *@*s: pointer
- *@b: value of assignment
+ *@b: value of assignmen
  *@n: end point
  *@*i: new value
  *Return: s
  */
 
 char *_memset(char *s, char b, unsigned int n)
+
 {
 	char *i = s;
+
 	while (n-- > 0)
 	{
 		*i++ = (unsigned char) b;
