b62d0ef47ac832de6bdbaa979e5717e6d04cfb14
nweiks <nweikeebube@gmail.com>
2023-04-08 22:20:09 +0000

    argsc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,9 +2,30 @@
 #include <stdio.h>
 
 /**
-*
-*
-*
+*main - maint string
+*@argc: counter
+@argv: vector
+*Return: 0 or 1 if statement are not up to 3
 */
 
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
 
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+
+if (num1 == 0 || num2 == 0)
+{
+printf("Error\n");
+return (1);
+}
+int result = num1 * num2;
+printf("%d\n", result);
+
+return (0);
+}
