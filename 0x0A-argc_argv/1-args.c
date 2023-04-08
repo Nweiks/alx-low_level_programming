606d31fd36d6813bf198db3f8de90bd8e6fa8709
nweiks <nweikeebube@gmail.com>
2023-04-08 08:13:39 +0000

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print argc
+ *@argc: count
+ *@argv: vector
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
