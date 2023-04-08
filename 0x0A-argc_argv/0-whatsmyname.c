4cb7146944e61092f046fbc81192ac206c61e2ad
nweiks <nweikeebube@gmail.com>
2023-04-08 08:03:27 +0000

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+*main - srg and argc conditions
+*@argv: argument vector
+*@argc: argument count
+*Return: 0 sucess
+*/
+
 int main(int argc, char *argv[])
 {
 	int i;
-		for(i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[0]);
-		}
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }
