3fec02b6e561a9cc9637ce173bb428dae52ccb53
nweiks <nweikeebube@gmail.com>
2023-04-09 10:53:16 +0000

    argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+*main - print argv
+*@argc: counter
+*@argv: vector
+*Return: 0
+*/
 
 int main(int argc, char *argv[])
 {
 int i;
-for (i = 1; i < argc; i++)
+for (i = 0; i < argc; i++)
 {
 printf("%s\n", argv[i]);
 }
