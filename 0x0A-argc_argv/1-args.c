b258a1e41a75c85dffac33a245b72162dbf626f9
nweiks <nweikeebube@gmail.com>
2023-04-09 10:41:35 +0000

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,13 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-int count = 0;
-
-for (i = 0; i < argc; i++)
-{
-count++;
-}
-	printf("%d\n", count - 1);
-	return (0);
+argv = argv;
+printf("%d\n", argc - 1);
+return (0);
 }
