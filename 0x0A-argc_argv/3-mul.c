c1ce67004f5bf7d1d2a72cd963fc213d3beb4542
nweiks <nweikeebube@gmail.com>
2023-04-10 16:17:28 +0000

    multipliy_fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,15 +13,15 @@ int main(int argc, char *argv[])
 	int i, j;
 
 	if (argc != 3)
-{
-printf("Error\n");
-return (1);
-}
-else
-{
-i = atoi(argv[1]);
-j = atoi(argv[2]);
-printf("%d\n", i *j);
-}
-return (0);
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		printf("%d\n", i * j);
+	}
+	return (0);
 }
