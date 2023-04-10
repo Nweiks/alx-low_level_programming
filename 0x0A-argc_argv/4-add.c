189faf70ef8d1ad9895364b30032cd47f8e19544
nweiks <nweikeebube@gmail.com>
2023-04-10 17:14:44 +0000

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,29 +2,37 @@
 #include <stdlib.h>
 
 /**
-*main - multiply numbers
-*@argc: count
-*@argv: vector
-*Return: 0
-*/
+ *main - multiply numbers
+ *@argc: count
+ *@argv: vector
+ *Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
-int sum = 0;
-int i, j;
-for (i = 1; i < argc; i++)
-{
+	int i;
+	int mul = 0;
+	int num;
+	int j;
+
+	for (i = 1; i < argc; i++)
+	{
 for (j = 0; argv[i][j]; j++)
 {
-if (!isdigit(argv[i][j]))
+		if (!isdigit(argv[i]))
+				{
+				printf("Error\n");
+				return (1);
+				}
+if (num < 0)
 {
 printf("Error\n");
 return (1);
 }
+				num = atoi(argv[i]);
+				mul += num;
 }
-int num = atoi(argv[i]);
-sum += num;
 }
-printf("%d\n", sum);
-return (0);
+				printf("%d\n", mul);
+				return (0);
 }
