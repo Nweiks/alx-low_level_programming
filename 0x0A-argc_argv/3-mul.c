3b83247c4e5ffd2768aeff9dcddb8fd248157003
nweiks <nweikeebube@gmail.com>
2023-04-10 16:01:05 +0000

    fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,7 +4,7 @@
 /**
  *main - maint string
  *@argc: counter
- @argv: vector
+ *@argv: vector
  *Return: 0 or 1 if statement are not up to 3
  */
 
@@ -12,16 +12,16 @@ int main(int argc, char *argv[])
 {
 	int i, j;
 
-	if (argc == 3)
+	if (argc != 3)
 {
-i = atoi(argv[1]);
-j = atoi(argv[2]);
-printf("%d\n", i * j);
+printf("Error\n");
+return (1);
 }
 else
 {
-	printf("Error\n");
-	return (1);
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+printf("%d\n", i *j);
 }
 return (0);
 }
