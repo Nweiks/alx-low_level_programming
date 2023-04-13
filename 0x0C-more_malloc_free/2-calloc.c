7bc8b6bbda352a83793806d99621b09443b4ace4
nweiks <nweikeebube@gmail.com>
2023-04-13 23:40:03 +0000

    allocate

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*_calloc - allocate memory 
+*@nmemb: string 
+*@size: size of string
+*Return: p
+*/
+
+void *_calloc(unsigned int memb, unsigned int size)
+{
+void *p;
+
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+p = malloc(nmemc * size);
+
+if (!p) 
+{
+return (NULL);
+}
+memset (p, 0,nmemb * size);
+return (p)
+}
