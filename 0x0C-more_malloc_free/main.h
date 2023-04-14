f04663b8a2e18fe4afd05ffd5736fb968c27a219
nweiks <nweikeebube@gmail.com>
2023-04-14 17:12:30 +0000

    mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
+int main(int argc, char *argv[]);
 
 #endif
