f12e9af7a827f193030dd4093e0a71712d26bb8f
nweiks <nweikeebube@gmail.com>
2023-04-18 23:47:32 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,11 +17,12 @@ char *owner;
 };
 
 /**
-*dog_t - typedef of dog 
+*dog_t - typedef of dog
 */
 
 typedef struct dog dog_t;
 
+dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
