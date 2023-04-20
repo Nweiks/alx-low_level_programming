3a56664de779de636b7227af7dde39982c59ebd7
nweiks <nweikeebube@gmail.com>
2023-04-20 23:33:20 +0000

    sum

--- 0x10-variadic_functions/varaidic_functions.h
+++ 0x10-variadic_functions/varaidic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARAIDIC_FUNCTIONS_H
-#define VARAIDIC_FUNCTIONS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
