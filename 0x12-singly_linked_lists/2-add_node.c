0a109fdae85bc117fb5a592bcadaf5dca9309658
Nweiks <nweikeebube@gmail.com>
2023-04-28 18:43:23 +0000

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,17 +19,12 @@ list_t *add_node(list_t **head, const char *str)
 	
 	new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL)
+	if (!new_node)
 	{
 		return (NULL);
 	}
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len =strlen(str);
-	new_node->len = len;
+
+	new_node->len = strlen(str);
 	new_node->next = (*head);
 	(*head) = new_node;
 	return (*head);
