00ae7bc464a81b38aad228a0441d23e8df62ec22
Nweiks <nweikeebube@gmail.com>
2023-04-28 19:01:57 +0000

    list_len fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,6 @@
-#include "lists.h"
 #include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
  *list_len - list the lenght
@@ -9,12 +10,12 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t c = 0;
+	size_t count = 0;
 
 	while (h)
 	{
-		c++;
+		count++;
 		h = h->next;
 	}
-	return (c);
+	return (count);
 }
