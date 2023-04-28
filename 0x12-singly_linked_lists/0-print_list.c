b0e154958ef6c6dfc4c0c037aae8ee143b598175
Nweiks <nweikeebube@gmail.com>
2023-04-28 01:07:31 +0000

    list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+*print_list - print list
+*@h: pointer 
+*Return: c
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t c;
+c = 0;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+h = h->next;
+c++;
+}
+return (c);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this is a readme for singly linked list project on alx
