6cbff844b610ad74ed1d69d5fe5d4ccf1730a835
Nweiks <nweikeebube@gmail.com>
2023-04-29 00:10:55 +0000

    64 bit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,19 @@
-global _start
-
 section .data
 hello db "Hello, Holberton", 10, 0
 
 section .text
-_start:
+global _start
+extern printf
 
-; set up arguments for printf
-mov rdi, hello
-xor rax, rax
-call printf
+_start:
+; Set up arguments for printf
+mov rdi, hello ; first argument: string address
+xor rax, rax ; clear rax register
+call printf ; call printf function
 
 ; Exit program
-xor edi, edi
-mov eax, 60
-syscall
+xor edi, edi ; set exit status to 0
+mov eax, 60 ; exit system call number
+syscall ; call system call
+
+
