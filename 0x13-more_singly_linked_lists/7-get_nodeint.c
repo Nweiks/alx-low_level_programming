c92adee3aeea0038f412234b1353ff5df618abc6
Nweiks <nweikeebube@gmail.com>
2023-04-30 00:13:32 +0000

    fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,32 +1,20 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
 *get_nodeint_at_index - get nth index
 *@head: headof node
 *@index: index to find
-*Return: NULL
+*Return: head
 */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-unsigned int i;
-listint_t c_node;
+unsigned int j;
 
-if (head == NULL)
+for (j = 0; head != NULL && j < index; j++)
 {
-return (NULL);
+head = head->next;
 }
-
-c_node = head;
-
-for (i = 0; i < index; i++)
-{
-if (c_node == NULL)
-{
-return (NULL);
-}
-c_node = c_node->next;
-}
-
-return (c_node);
+return (head);
 }
