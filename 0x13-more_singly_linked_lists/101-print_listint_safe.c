cd7d59c249f8b9b1e6feee12da92e7708eb7b9bc
Nweiks <nweikeebube@gmail.com>
2023-05-03 18:11:41 +0000

    next

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,7 +12,7 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
+	const listint_t *current = head, *next;
 	size_t count = 0;
 
 	while (current)
@@ -20,13 +20,14 @@ size_t print_listint_safe(const listint_t *head)
 		count++;
 		printf("[%p] %d\n", (void *)current, current->n);
 
+		next = current->next;
 
-		if (current->next >= current)
+		if (next >= current)
 		{
 			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
 			exit(98);
 		}
-		current = current->next;
+		current = next;
 	}
 	return (count);
 }
